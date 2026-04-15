9232dbf3fd7abe644d9f6543edd38843184891d1
DGRAG0N <fung.lucas609@gmail.com>
2026-04-15 16:18:21 -0700

    Fix saving image data in split_anno.groovy
    Fixed syntax error in saving image data.

--- scripts/split_anno.groovy
+++ scripts/split_anno.groovy
@@ -1,6 +1,11 @@
-///split annotations
+//Get arguments
+def className = args.length > 0 ? args[0] : "Anthracosis"
+
+//Split annotations
 selectObjectsByClassification("Anthracosis")
 runPlugin('qupath.lib.plugins.objects.SplitAnnotationsPlugin', '{}')
 
-getProject().getEntry(getCurrentImageData()).saveImageData()
+//Save image data
+def imageData = getCurrentImageData()
+getProject().getEntry(imageData)saveImageData()
 println "Anthracosis annotations split!"
